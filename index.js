80ed9f131c7deae49ccab28933d3372a7c74a571
Truimo <xiaomo@20mo.cn>
2023-03-30 11:00:09 +0800

    refactor: modify the method of import package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-const {createProxyMiddleware} = require('http-proxy-middleware')
+import express from "express";
+import cors from "cors";
+import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express()
 
 app.use(cors())
